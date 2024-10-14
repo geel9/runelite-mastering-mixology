a4ff245cc0db4d21b7c70c411ac5ce0daf257b77
Joshua Coffey <josh@trinsic.id>
2024-10-14 10:34:52 -0400

    Remove unused config

--- src/main/java/com/geel/masteringmixology/MasteringMixologyConfig.java
+++ src/main/java/com/geel/masteringmixology/MasteringMixologyConfig.java
@@ -9,44 +9,44 @@ import java.awt.*;
 @ConfigGroup(MasteringMixologyConfig.GROUP)
 public interface MasteringMixologyConfig extends Config {
     String GROUP = "masteringmixology";
-
-    @ConfigItem(
-            position = 0,
-            keyName = "prioritization",
-            name = "Prioritization",
-            description = "How to prioritize tasks: none, by point ratio, or by a specific building type"
-    )
-    default PriorityMethod prioritization() {
-        return PriorityMethod.NONE;
-    }
-
-    @ConfigItem(
-            position = 1,
-            keyName = "moxGoal",
-            name = "Mox Goal",
-            description = "Goal mox points"
-    )
-    default int moxGoal() {
-        return 0;
-    }
-
-    @ConfigItem(
-            position = 2,
-            keyName = "agaGoal",
-            name = "Aga Goal",
-            description = "Goal aga points"
-    )
-    default int agaoGoal() {
-        return 0;
-    }
-
-    @ConfigItem(
-            position = 3,
-            keyName = "lyeGoal",
-            name = "Lye Goal",
-            description = "Goal lye points"
-    )
-    default int lyeGoal() {
-        return 0;
-    }
+//
+//    @ConfigItem(
+//            position = 0,
+//            keyName = "prioritization",
+//            name = "Prioritization",
+//            description = "How to prioritize tasks: none, by point ratio, or by a specific building type"
+//    )
+//    default PriorityMethod prioritization() {
+//        return PriorityMethod.NONE;
+//    }
+//
+//    @ConfigItem(
+//            position = 1,
+//            keyName = "moxGoal",
+//            name = "Mox Goal",
+//            description = "Goal mox points"
+//    )
+//    default int moxGoal() {
+//        return 0;
+//    }
+//
+//    @ConfigItem(
+//            position = 2,
+//            keyName = "agaGoal",
+//            name = "Aga Goal",
+//            description = "Goal aga points"
+//    )
+//    default int agaoGoal() {
+//        return 0;
+//    }
+//
+//    @ConfigItem(
+//            position = 3,
+//            keyName = "lyeGoal",
+//            name = "Lye Goal",
+//            description = "Goal lye points"
+//    )
+//    default int lyeGoal() {
+//        return 0;
+//    }
 }
